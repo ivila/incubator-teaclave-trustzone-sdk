2c5206a96fdb025ab6274477ec6c744fad297c3e
ivila <390810839@qq.com>
2025-01-20 15:14:14 +0800

    examples: update tcp_client-rs and udp_socket-rs to support no_std
    - add ipv6 call in tcp_client-rs and udp_socket-rs
    - enable no_std run of tcp_client-rs and udp_socket-rs in ci
    
    Signed-off-by: ivila <390810839@qq.com>
    Reviewed-by: Yuan Zhuang <yuanz@apache.org>

--- examples/tcp_client-rs/host/src/main.rs
+++ examples/tcp_client-rs/host/src/main.rs
@@ -15,12 +15,27 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use optee_teec::ParamNone;
-use optee_teec::{Context, Operation, Session, Uuid};
-use proto::{Command, UUID};
+use std::net::{SocketAddr, ToSocketAddrs};
 
-fn tcp_client(session: &mut Session) -> optee_teec::Result<()> {
-    let mut operation = Operation::new(0, ParamNone, ParamNone, ParamNone, ParamNone);
+use optee_teec::{Context, Operation, ParamType, Session, Uuid};
+use optee_teec::{ParamNone, ParamTmpRef, ParamValue};
+use proto::{Command, IpVersion, UUID};
+
+fn tcp_client(
+    session: &mut Session,
+    address: &str,
+    port: u16,
+    ip_version: IpVersion,
+    host_name: &str,
+) -> optee_teec::Result<()> {
+    let http_data = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", host_name);
+    let mut operation = Operation::new(
+        0,
+        ParamTmpRef::new_input(address.as_bytes()),
+        ParamValue::new(port as u32, ip_version as u32, ParamType::ValueInput),
+        ParamTmpRef::new_input(http_data.as_bytes()),
+        ParamNone,
+    );
     session.invoke_command(Command::Start as u32, &mut operation)?;
     Ok(())
 }
@@ -30,8 +45,30 @@ fn main() -> optee_teec::Result<()> {
     let uuid = Uuid::parse_str(UUID).unwrap();
     let mut session = ctx.open_session(uuid)?;
 
-    tcp_client(&mut session)?;
+    // test ipv4
+    const IPV4_HOST: &str = "teaclave.apache.org";
+    tcp_client(&mut session, IPV4_HOST, 80, IpVersion::V4, IPV4_HOST)?;
+    // test ipv6
+    const IPV6_HOST: &str = "ipv6.google.com";
+    let addr = resolve_ipv6_addr(format!("{}:80", IPV6_HOST).as_str())?;
+    tcp_client(
+        &mut session,
+        addr.ip().to_string().as_str(),
+        addr.port(),
+        IpVersion::V6,
+        IPV6_HOST,
+    )?;
 
     println!("Success");
     Ok(())
+}
+
+// making sure we get an ipv6 address
+fn resolve_ipv6_addr(domain: &str) -> optee_teec::Result<SocketAddr> {
+    for addr in domain.to_socket_addrs().unwrap() {
+        if addr.is_ipv6() {
+            return Ok(addr.clone());
+        }
+    }
+    Err(optee_teec::ErrorKind::BadState.into())
 }
